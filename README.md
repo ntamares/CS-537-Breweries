109e3f8e0954e8a47045575df8ebc9e9ba1ab072
Nestor Tamares <jrtamares@gmail.com>
2016-11-29 17:24:28 -0800

    Updated README, Added main file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# CS-537-Breweries
+# CS 537 - Breweries
+
+Nestor Tamares  
+masc2566  
+CS 537 - Programming for GIS  
+Prof. Eckberg  
+  
+This project will show a map of San Diego with several different craft beer  
+breweries as the points of interest. Upon clicking on one of the breweries,  
+a window will pop up containing information about that certain breweries such  
+as location, types of beer that they carry, and the company logo.
---
+++ src/Breweries.java
@@ -0,0 +1,39 @@
+/*
+
+Nestor Tamares
+masc2566
+CS 537 - Programming for GIS
+Prof. Eckberg
+
+*/
+
+import javax.swing.*;
+import java.io.IOException;
+import java.awt.event.*;
+import java.awt.*;
+import com.esri.mo2.ui.bean.*;
+import com.esri.mo2.ui.tb.ZoomPanToolBar;
+import com.esri.mo2.ui.tb.SelectionToolBar;
+import com.esri.mo2.ui.ren.LayerProperties;
+import com.esri.mo2.cs.geom.Envelope;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableColumn;
+import com.esri.mo2.data.feat.*; 
+import com.esri.mo2.map.dpy.FeatureLayer;
+import com.esri.mo2.file.shp.*;
+import com.esri.mo2.map.dpy.Layerset;
+import com.esri.mo2.map.draw.*;
+import com.esri.mo2.ui.bean.Tool;
+import java.util.ArrayList;
+
+public class Breweries extends JFrame{
+	
+	public Breweries(){
+		
+	}
+	
+}
+
+public static void main(String[] args){
+	return 0;
+}
